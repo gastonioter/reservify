ca60fb2e03801503c54c5326db4b9fb32c6ac0fd
gastonioter <iotting.castro10@gmail.com>
2024-03-07 01:42:00 -0300

    define Toaster

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 //jPvDqODtBCZ15qQ6: db password
 
 const queryClient = new QueryClient({
@@ -24,6 +25,25 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            backgroundColor: "var(--color-gray-0)",
+            color: "var(--color-gray-700)",
+          },
+        }}
+      />
       <ReactQueryDevtools initialIsOpen={false} />
 
       <GlobalStyles />
