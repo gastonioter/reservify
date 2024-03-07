72ffb1918a6318fb81d5ef62388ff7fba856a1af
gastonioter <iotting.castro10@gmail.com>
2024-03-07 00:55:04 -0300

    staleTime

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 //jPvDqODtBCZ15qQ6: db password
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 function App() {
   return (
